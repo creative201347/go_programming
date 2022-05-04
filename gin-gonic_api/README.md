640f7ef7ec6bc463e5a54c3a2d8123cfcbb9839d
creative201347 <creative201347@gmail.com>
2022-05-04 08:02:52 +0545

    gin-gonic_api

---
+++ gin-gonic_api/README.md
@@ -0,0 +1,11 @@
+# Building a REST API with Go and MongoDB
+
+## Technologies
+
+- Go
+- MongoDB with mongo.driver
+
+## Dependencies
+
+- go get go.mongodb.org/mongo-driver
+- go get github.com/gin-gonic/gin
---
+++ gin-gonic_api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/creative201347/controllers"
+	"github.com/creative201347/services"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server         *gin.Engine
+	userservice    services.UserService
+	usercontroller controllers.UserController
+	ctx            context.Context
+	usercollection *mongo.Collection
+	mongoclient    *mongo.Client
+	err            error
+)
+
+func init() {
+	ctx = context.TODO()
+	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoclient, err = mongo.Connect(ctx, mongoconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("successfully connected to the database")
+	usercollection = mongoclient.Database("userdb").Collection("users")
+	userservice = services.NewUserService(usercollection, ctx)
+	usercontroller = controllers.New(userservice)
+	server = gin.Default()
+}
+
+func main() {
+
+	defer mongoclient.Disconnect(ctx)
+	basepath := server.Group("/v1")
+	usercontroller.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
+}
